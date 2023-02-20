65bbfe53453c68bfdfdb5cd30d8838bbd02ef8d8
jeremyzz830 <zjm731215@outlook.com>
2023-02-20 17:13:24 -0500

    --

---
+++ sub_pose.py
@@ -0,0 +1,24 @@
+#!/use/env/bin python
+
+import rospy
+from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import Pose
+
+def callback(msg):
+
+    pass
+
+def plot_interp_3D():
+    pass
+
+
+def main();
+    
+        rospy.init_node('sub_pose')
+        rospy.Subscriber('/NDI_plot', PoseStamped, callback)
+        rospy.spin()
+
+
+if __name__ == '__main__';
+    main()
+    
